01b58376d1224909acd6a361571e844ce037560b
Keith Tomlinson <kbarretttomlinson@gmail.com>
2019-05-25 16:36:29 -0500

    sets up grunt to copy and watch

---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'client/views',
+        src: ['index.html',
+              '**/*.html',
+              '**/**/*.html'],
+        dest: 'server/public/views/'
+      },
+      scripts: {
+        expand: true,
+        cwd: 'client/scripts',
+        src: ['client.js','*.js','**/*.js'],
+        dest: 'server/public/scripts/'
+      },
+      css: {
+        expand: true,
+        cwd: 'client/styles',
+        src: ['style.css','images/*.*'],
+        dest: 'server/public/styles/'
+      },
+      angular: {
+        expand: true,
+        cwd: 'node_modules/angular/',
+        src: ['angular.js',
+              'angular.min.js',
+              'angular.min.js.map'],
+        dest: 'server/public/vendors/angular/'
+      }
+    },
+    watch: {
+      files: [
+        'client/**/*.*'
+      ],
+      tasks: ['copy']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['copy','watch']);
+};
