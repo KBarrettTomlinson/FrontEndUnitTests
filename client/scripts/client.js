ddbe545a4c9e648784486074461713638bf28912
Keith Tomlinson <kbarretttomlinson@gmail.com>
2019-05-25 19:41:49 -0500

    sets up basic angular app with input and button which causes logic to happen on the input, reutnrning a value

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,1 +1,35 @@
-console.log("client.js loaded");
+const app = angular.module( 'app', []);
+
+app.controller('UnitTestController', ['$scope', 'UnitTestService', function($scope, UnitTestService){
+  $scope.inputValue;
+  $scope.submitInput = function(inputValue){
+    $scope.outputValue = UnitTestService.submitInput(inputValue);
+  };
+}]);
+
+app.service('UnitTestService', [function(){
+  this.submitInput = function(inputValue){
+    return getOutputValue(inputValue);
+  };
+
+  const getOutputValue = function(inputValue){
+    const outputValue = {};
+    outputValue.value1 = getValue1(inputValue);
+    outputValue.value2 = getValue2(inputValue);
+    return outputValue;
+  }
+
+  const getValue1 = function(inputValue){
+    if(typeof inputValue === 'undefined'){
+      return "undefined input value";
+    } else {
+      return inputValue + 1;
+    }
+  }
+
+  const getValue2 = function(inputValue){
+    return inputValue + 2;
+  }
+
+
+}]);
